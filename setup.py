2ec2c95efa347c507ebf08b5e8f38f8ba2b133fd
Yiren Jian <32028985+yiren-jian@users.noreply.github.com>
2022-01-18 15:20:06 -0500

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name='META_DOMAIN_TRANSFER',
+setup(name='MetaPix',
       install_requires=['pyyaml', 'tensorboardX',
                         'easydict', 'matplotlib',
                         'scipy', 'scikit-image',
